1b5bab1a3779ec1d111ae2b75883c4a1ab5d10d2
Nico Ramirez <ramireznicc.23@gmail.com>
2023-06-07 17:31:48 -0300

    adding some features

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,38 +1,49 @@
-import { useState, useEffect } from "react";
-import { IconButton, TextField, Typography, Stack, Box } from "@mui/material";
+import { useState } from "react";
+import {
+  IconButton,
+  TextField,
+  Typography,
+  Stack,
+  Box,
+  Slide,
+} from "@mui/material";
 import { Icons } from "../Icons/Icons";
 import { Search } from "@mui/icons-material";
 import axios from "axios";
 
 export const Home = () => {
   const [data, setData] = useState({
-    name: "London",
-    country: "UK",
-    temp: 10,
-    wind: 35,
-    humidity: 75,
+    name: "",
+    country: "",
+    temp: 0,
+    weather: "",
+    wind: 0,
+    humidity: 0,
   });
+  const [city, setCity] = useState("");
 
-  const [city, setCity] = useState("Rosario");
-  const [userInput, setUserInput] = useState("");
-
-  useEffect(() => {
+  const handleSearch = () => {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=277e668a19c681cb57cf26eec78b2b4d&&units=metric`;
-
     axios
       .get(apiUrl)
-      .then((res) =>
+      .then((res) => {
         setData({
           ...data,
           name: res.data.name,
           country: res.data.sys.country,
           temp: res.data.main.temp,
+          weather: res.data.weather[0].main,
           wind: (res.data.wind.speed * 3.6).toFixed(1),
           humidity: res.data.main.humidity,
-        })
-      )
+        });
+      })
       .catch((err) => console.log(err));
-  });
+    setCity("");
+  };
+
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
 
   return (
     <>
@@ -47,92 +58,98 @@ export const Home = () => {
       </Typography>
       <Stack direction="row" alignItems="center" gap="12px" width="100%">
         <TextField
+          value={city}
           color="primary"
           id="city"
           size="small"
           variant="outlined"
           label="Enter city name"
           fullWidth={true}
-          onChange={(e) => {
-            setUserInput(e.target.value);
-          }}
           sx={{ input: { color: "text.light" } }}
+          onChange={handleChange}
         />
-        <IconButton color="primary" onClick={() => setCity(userInput)}>
+        <IconButton color="primary" onClick={handleSearch}>
           <Search />
         </IconButton>
       </Stack>
-      <Box
-        my="16px"
-        sx={{
-          maxWidth: "500px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "space-evenly",
-          backgroundColor: "background.paper",
-          width: "100%",
-          borderRadius: "12px",
-          boxShadow: "0 2px 4px rgba(0, 0, 0, 0.3)",
-          p: "16px",
-          gap: "16px",
-        }}
-      >
-        <Typography
-          color="text.dark"
-          variant="h4"
-          fontWeight={600}
-          letterSpacing=".1rem"
-        >
-          {data.name}, {data.country}
-        </Typography>
-        <Icons.Sun sx={{ fontSize: "10rem" }} color="primary" />
-        <Typography letterSpacing=".2rem" variant="h3" fontWeight={700}>
-          {data.temp.toFixed(1)}°
-        </Typography>
-        <Stack direction="row" gap="22px">
-          <Stack
-            direction="row"
-            alignItems="center"
-            gap="6px"
-            justifyContent="center"
+      {data.name === "" ? null : (
+        <Slide timeout={1000} direction="up" mountOnEnter in={true}>
+          <Box
+            my="16px"
+            sx={{
+              maxWidth: "500px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "space-evenly",
+              backgroundColor: "background.paper",
+              width: "100%",
+              borderRadius: "12px",
+              boxShadow: "0 2px 4px rgba(0, 0, 0, 0.3)",
+              p: "16px",
+              gap: "16px",
+            }}
           >
-            <Icons.Humidity color="primary" sx={{ fontSize: "2.6rem" }} />
-            <Stack direction="column" alignItems="center">
-              <Typography lineHeight="22px" variant="h6">
-                {data.humidity}%
-              </Typography>
-              <Typography
-                lineHeight="22px"
-                fontWeight={600}
-                letterSpacing=".1rem"
+            <Typography
+              color="text.dark"
+              variant="h4"
+              fontWeight={600}
+              letterSpacing=".1rem"
+            >
+              {data.name}, {data.country}
+            </Typography>
+            <Typography variant="h5" fontWeight={500}>
+              {data.weather}
+            </Typography>
+            <Icons iconName={data.weather} fontSize="9.4rem" />
+            <Typography letterSpacing=".2rem" variant="h3" fontWeight={700}>
+              {data.temp.toFixed(1)}°
+            </Typography>
+            <Stack direction="row" gap="4rem">
+              <Stack
+                direction="row"
+                alignItems="center"
+                gap="1rem"
+                justifyContent="center"
               >
-                Humidity
-              </Typography>
-            </Stack>
-          </Stack>
-          <Stack
-            direction="row"
-            alignItems="center"
-            gap="6px"
-            justifyContent="center"
-          >
-            <Icons.Wind color="primary" sx={{ fontSize: "2.6rem" }} />
-            <Stack direction="column" alignItems="center">
-              <Typography lineHeight="22px" variant="h6">
-                {data.wind} km/h
-              </Typography>
-              <Typography
-                lineHeight="22px"
-                fontWeight={600}
-                letterSpacing=".1rem"
+                <Icons iconName="Humidity" fontSize="2.6rem" />
+                <Stack direction="column" alignItems="center">
+                  <Typography lineHeight="22px" variant="h6">
+                    {data.humidity}%
+                  </Typography>
+                  <Typography
+                    lineHeight="22px"
+                    fontWeight={600}
+                    letterSpacing=".1rem"
+                  >
+                    Humidity
+                  </Typography>
+                </Stack>
+              </Stack>
+              <Stack
+                direction="row"
+                alignItems="center"
+                gap="1rem"
+                justifyContent="center"
               >
-                Wind
-              </Typography>
+                <Icons iconName="Wind" fontSize={"2.6rem"} />
+                <Stack direction="column" alignItems="center">
+                  <Typography lineHeight="22px" variant="h6">
+                    {data.wind} km/h
+                  </Typography>
+                  <Typography
+                    lineHeight="22px"
+                    fontWeight={600}
+                    letterSpacing=".1rem"
+                  >
+                    Wind
+                  </Typography>
+                </Stack>
+              </Stack>
             </Stack>
-          </Stack>
-        </Stack>
-      </Box>
+          </Box>
+        </Slide>
+      )}
     </>
   );
 };
---
+++ src/components/Icons/Icons.jsx
@@ -0,0 +1,46 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCloud,
+  faCloudBolt,
+  faCloudRain,
+  faUmbrella,
+  faSnowflake,
+  faSun,
+  faSmog,
+  faHurricane,
+  faDroplet,
+  faWind,
+  faCircleExclamation,
+} from "@fortawesome/free-solid-svg-icons";
+
+export const Icons = ({ iconName, fontSize }) => {
+  const weather = [
+    { name: "Thunderstorm", icon: faCloudBolt },
+    { name: "Clouds", icon: faCloud },
+    { name: "Drizzle", icon: faUmbrella },
+    { name: "Rain", icon: faCloudRain },
+    { name: "Snow", icon: faSnowflake },
+    { name: "Clear", icon: faSun },
+    { name: "Mist", icon: faSmog },
+    { name: "Smoke", icon: faSmog },
+    { name: "Haze", icon: faSmog },
+    { name: "Dust", icon: faSmog },
+    { name: "Fog", icon: faSmog },
+    { name: "Sand", icon: faSmog },
+    { name: "Ash", icon: faSmog },
+    { name: "Squall", icon: faSmog },
+    { name: "Tornado", icon: faHurricane },
+    { name: "Humidity", icon: faDroplet },
+    { name: "Wind", icon: faWind },
+    { name: "error", icon: faCircleExclamation },
+  ];
+  const item = weather.find((item) => item.name === iconName);
+
+  return (
+    <FontAwesomeIcon
+      icon={iconName !== "" ? item.icon : faCircleExclamation}
+      fontSize={fontSize}
+      color="#ff2c55"
+    />
+  );
+};
